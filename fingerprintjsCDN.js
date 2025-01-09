6fe27eced5412289d1e7909438868509331a5ad7
NaziaAfreen015 <nazia.afreen015@gmail.com>
2025-01-09 18:36:41 -0500

    fingerprintjs from CDN added

---
+++ fingerprintjsCDN.js
@@ -0,0 +1,18 @@
+async function collectAndDisplayFingerprintData() {
+    const fpPromise = import('https://openfpcdn.io/fingerprintjs/v4')
+          .then(FingerprintJS => FingerprintJS.load())
+      
+        // Get the visitor identifier when you need it.
+        fpPromise
+          .then(fp => fp.get())
+          .then(result => {
+            // This is the visitor identifier:
+            const visitorId = result.visitorId
+            console.log(visitorId)
+            document.getElementById("hashedData").textContent = visitorId;
+          })
+
+}
+
+// Call the function to collect and display the data
+collectAndDisplayFingerprintData();
